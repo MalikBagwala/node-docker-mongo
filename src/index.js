34d8be330b3237cf50ac0cef4509f39580634f2c
MalikBagwala <m.bagwala@gmail.com>
2020-04-05 19:22:05 +0530

    added support for authentication and authorization

--- src/index.js
+++ src/index.js
@@ -1,16 +1,48 @@
 const express = require("express");
 const mongoose = require("mongoose");
+import bodyParser from "body-parser";
 import userRouter from "./user/user.routes";
+import User from "./user/user.model";
+import jwt from "jsonwebtoken";
+require("dotenv").config();
 const app = express();
 mongoose.connect("mongodb://mongo:27017/app", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
+// Middlewares
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Routes
 app.use("/users", userRouter);
 app.get("/", (_, res) => {
   res.send("Home");
 });
+
+app.post("/login", async (req, res) => {
+  const credentials = {
+    email: req.body?.email,
+    password: req.body?.password,
+  };
+  try {
+    const user = await User.findOne(credentials);
+    jwt.sign(credentials, process.env.SECRET_KEY, (err, token) => {
+      res.status(200).json({
+        status: 200,
+        data: { user, token },
+      });
+    });
+  } catch (error) {
+    res.status(401).json({
+      status: 401,
+      data: null,
+    });
+  }
+});
+
+// Start Server Here
 app.listen(8000, () => {
   console.log("Server Running : 8000");
 });
--- src/user/user.routes.js
+++ src/user/user.routes.js
@@ -3,12 +3,19 @@ import User from "./user.model";
 const userRouter = express.Router();
 
 userRouter.get("/", (_, res) => {
-  User.find().then((users) =>
-    res.json({
-      status: "200",
-      data: users,
-    })
-  );
+  User.find()
+    .then((users) =>
+      res.status(200).json({
+        status: "200",
+        data: users,
+      })
+    )
+    .catch((err) =>
+      res.status(404).json({
+        status: "404",
+        data: [],
+      })
+    );
 });
 
 userRouter.get("/:id", (req, res) => {
@@ -29,8 +36,19 @@ userRouter.get("/:id", (req, res) => {
 });
 
 userRouter.post("/", (req, res) => {
-  User.findById();
-  console.log(req);
+  User.create(req.body)
+    .then((user) =>
+      res.status(201).json({
+        status: 201,
+        data: user,
+      })
+    )
+    .catch((err) =>
+      res.status(400).json({
+        status: 400,
+        data: null,
+      })
+    );
 });
 
 export default userRouter;
